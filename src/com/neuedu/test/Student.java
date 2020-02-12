48d40d88863cb44d50b8770684137354ce6d3856
simeng1997 <603851600@qq.com>
2020-02-12 15:40:06 +0800

    test6

---
+++ src/com/neuedu/test/Student.java
@@ -0,0 +1,40 @@
+package com.neuedu.test;
+
+public class Student {
+    private Integer id;
+    private String name;
+    private int age;
+    private String grade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
