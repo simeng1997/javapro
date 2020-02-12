037060d275cc87fcd596f95767d4cf4cf3d2c11c
simeng1997 <603851600@qq.com>
2020-02-12 09:56:45 +0800

    test5

---
+++ src/com/neuedu/test/MyBookMain.java
@@ -0,0 +1,37 @@
+package com.neuedu.test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MyBookMain {
+//    public static MyBook[] array= new MyBook[200];
+//    public static List<MyBook> list = new ArrayList<>();
+//    public static void main(String[] args) {
+//        inputData(list);
+//        print(list);
+//        searchName(list,"书名9");
+//    }
+//    public static void inputData(List<MyBook> books){
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//        for (int i = 1;i<=30;i++){
+//            MyBook myBook = new MyBook("书名"+i,Double.valueOf(String.valueOf(i)),"出版社"+i,"作者"+i,"ISBN"+i);
+//            books.add(myBook);
+//        }
+//    }
+//    public static void print(List<MyBook> books){
+//        for (int i=0;i<books.size();i++){
+//            System.out.println(books.get(i));
+//        }
+//    }
+//    public static void searchName(List<MyBook> books,String name){
+//        for (int i=0;i<books.size();i++){
+//            MyBook myBook = books.get(i);
+//            if (myBook.getName().equals(name)){
+//                System.out.println(myBook);
+//                break;
+//            }
+//        }
+//        System.out.println("此书不存在");
+//    }
+}
