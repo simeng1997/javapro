57665ac0b79a4b17cd006c9ba0e73d284d9d8171
simeng1997 <603851600@qq.com>
2020-02-12 22:19:35 +0800

    test6

--- src/com/neuedu/test/Student.java
+++ src/com/neuedu/test/Student.java
@@ -1,6 +1,9 @@
 package com.neuedu.test;
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
     private Integer id;
     private String name;
     private int age;
